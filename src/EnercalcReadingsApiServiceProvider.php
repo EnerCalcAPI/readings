1bc57424e5f99af66b4d0fb2bf545ca8e1a595bb
virgilBE <virgil@bouwenergie.nl>
2021-08-10 13:03:41 +0200

    Initial Commit

---
+++ src/EnercalcReadingsApiServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Enercalcapi\Readings;
+
+use Illuminate\Support\ServiceProvider;
+
+class EnercalcReadingsApiServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+        
+    }
+
+}
